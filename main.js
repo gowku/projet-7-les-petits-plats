c19bff50a12d99e3e83470603f73c1ef2463df28
Julien Frebet <frebet.julien@gmail.com>
2022-09-20 18:27:13 +0200

    html et css ok

--- main.js
+++ main.js
@@ -1,27 +1,33 @@
-const dropdown = document.querySelector(".dropdown");
-const list = document.querySelector(".dropdown__list");
-const listContainer = document.querySelector(".dropdown__list-container");
-const dropdownArrow = document.querySelector(".dropdown__arrow");
+const dropdown = document.querySelectorAll(".dropdown");
+const list = document.querySelectorAll(".dropdown__list");
+const listContainer = document.querySelectorAll(".dropdown__list-container");
+const dropdownArrow = document.querySelectorAll(".dropdown__arrow");
 const listItems = document.querySelectorAll(".dropdown__list-item");
-const toRemove = document.querySelector("#dropdown__selected");
-const dropdownSelectedNode = document.querySelector(".selected_item");
+const dropdownSelectedNode = document.querySelectorAll(".dropdown__arrow");
 const listItemIds = [];
+// const toRemove = document.querySelector("#dropdown__selected");
 
-const p = `<p id="dropdown__selected">Ingrédients</p>`;
-const input = `<input id="input_selected" placeholder="Rechercher un ingrédient"></input>`;
+for (let i = 0; i < dropdownSelectedNode.length; i++) {
+  dropdownSelectedNode[i].addEventListener("click", (e) => {
+    list[i].classList.toggle("open");
+    dropdown[i].classList.toggle("radius");
+    dropdown[i].classList.toggle("open_input");
+    dropdownArrow[i].classList.toggle("expanded");
+    listContainer[i].setAttribute("aria-expanded", list[i].classList.contains("open"));
+  });
+}
 
-dropdownSelectedNode.addEventListener("click", (e) => {
-  toRemove.remove();
-  dropdownSelectedNode.innerHTML = input;
-  //   const input = document.createElement("input");
-  //   dropdownSelectedNode.appendChild(input);
-  list.classList.toggle("open");
-
-  dropdown.classList.toggle("radius");
-  dropdown.classList.toggle("open_input");
-  dropdownArrow.classList.toggle("expanded");
-  listContainer.setAttribute("aria-expanded", list.classList.contains("open"));
-});
+// dropdownSelectedNode.addEventListener("click", (e) => {
+//   // toRemove.remove();
+//   // dropdownSelectedNode.innerHTML = input;
+//   //   const input = document.createElement("input");
+//   //   dropdownSelectedNode.appendChild(input);
+//   list.classList.toggle("open");
+//   dropdown.classList.toggle("radius");
+//   dropdown.classList.toggle("open_input");
+//   dropdownArrow.classList.toggle("expanded");
+//   listContainer.setAttribute("aria-expanded", list.classList.contains("open"));
+// });
 
 listItems.forEach((item) => listItemIds.push(item.id));
 
@@ -33,17 +39,17 @@ listItems.forEach((item) => {
   });
 });
 
-function setSelectedListItem(e) {
-  let selectedTextToAppend = document.createTextNode(e.target.innerText);
-  dropdownSelectedNode.innerHTML = null;
-  dropdownSelectedNode.appendChild(selectedTextToAppend);
-  // console.log(selectedTextToAppend);
-}
+// function setSelectedListItem(e) {
+//   let selectedTextToAppend = document.createTextNode(e.target.innerText);
+//   dropdownSelectedNode.innerHTML = null;
+//   dropdownSelectedNode.appendChild(selectedTextToAppend);
+//   // console.log(selectedTextToAppend);
+// }
 
 function closeList() {
   list.classList.remove("open");
-  toRemove.remove();
-  dropdownSelectedNode.innerHTML = p;
+  // toRemove.remove();
+  // dropdownSelectedNode.innerHTML = p;
   dropdownArrow.classList.remove("expanded");
   listContainer.setAttribute("aria-expanded", false);
 }
